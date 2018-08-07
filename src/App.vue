640e1ac6ad80ed4c2c78e502ad9e9406e222daa3
fengpang <fengpang631663208@gamil.com>
2018-08-07 15:42:22 +0800

    ...

--- src/App.vue
+++ src/App.vue
@@ -23,4 +23,5 @@ export default {
 
 <style lang='stylus'>
   @import "style/common"
+  @import "style/mixin"
 </style>
--- src/components/personal/PersonalPage.vue
+++ src/components/personal/PersonalPage.vue
@@ -2,7 +2,11 @@
   <div class="hello">
     <h1>{{ msg }}</h1>
     <h2>PersonalPage</h2>
-    <mt-cell title="标题文字"></mt-cell>
+    <mt-cell title="标题文字" value="说明文字"></mt-cell>
+    <mt-cell title="标题文字" value="说明文字"></mt-cell>
+    <mt-cell title="标题文字" value="说明文字"></mt-cell>
+    <mt-cell title="标题文字" value="说明文字"></mt-cell>
+    <mt-cell title="标题文字" value="说明文字"></mt-cell>
     <mt-cell title="标题文字" value="说明文字"></mt-cell>
     <mt-cell title="标题文字" icon="more" value="带 icon"></mt-cell>
   </div>
