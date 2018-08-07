da80e2101e6922765a7c03edcfbd563ce0e17a13
fengpang <fengpang631663208@gamil.com>
2018-08-07 13:52:15 +0800

    引入公共样式

--- src/App.vue
+++ src/App.vue
@@ -22,13 +22,5 @@ export default {
 </script>
 
 <style lang='stylus'>
-// @import 'style/common';
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  @import "style/common"
 </style>
